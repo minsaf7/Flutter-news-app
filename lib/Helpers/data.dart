fc3a74b123f5628ef4dcdf6ae3c3969257251390
Mohamed Minsaaf <minsafmohamed7@gmail.com>
2021-07-14 11:44:36 +0530

    News category UI

---
+++ lib/Helpers/data.dart
@@ -0,0 +1,78 @@
+import 'package:flutternews/Models/CategoryModel.dart';
+
+List<CategoryModel> getCategory() {
+  List<CategoryModel> category = [];
+
+  CategoryModel model = CategoryModel();
+  //1
+  model.categoryTitle = "Business";
+  model.imageUrl =
+      "https://gordontredgold.com/wp-content/uploads/2018/08/business.jpg";
+
+  category.add(model);
+
+//2
+  model = CategoryModel();
+
+  model.categoryTitle = "Business";
+  model.imageUrl =
+      "https://gordontredgold.com/wp-content/uploads/2018/08/business.jpg";
+
+  category.add(model);
+
+//3
+  model = CategoryModel();
+
+  model.categoryTitle = "Business";
+  model.imageUrl =
+      "https://gordontredgold.com/wp-content/uploads/2018/08/business.jpg";
+
+  category.add(model);
+
+  //4
+  model = CategoryModel();
+
+  model.categoryTitle = "Business";
+  model.imageUrl =
+      "https://gordontredgold.com/wp-content/uploads/2018/08/business.jpg";
+
+  category.add(model);
+
+  //5
+  model = CategoryModel();
+
+  model.categoryTitle = "Business";
+  model.imageUrl =
+      "https://gordontredgold.com/wp-content/uploads/2018/08/business.jpg";
+
+  category.add(model);
+
+  //6
+  model = CategoryModel();
+
+  model.categoryTitle = "Business";
+  model.imageUrl =
+      "https://gordontredgold.com/wp-content/uploads/2018/08/business.jpg";
+
+  category.add(model);
+
+  //7
+  model = CategoryModel();
+
+  model.categoryTitle = "Business";
+  model.imageUrl =
+      "https://gordontredgold.com/wp-content/uploads/2018/08/business.jpg";
+
+  category.add(model);
+
+  //8
+  model = CategoryModel();
+
+  model.categoryTitle = "Business";
+  model.imageUrl =
+      "https://gordontredgold.com/wp-content/uploads/2018/08/business.jpg";
+
+  category.add(model);
+
+  return category;
+}
--- lib/Views/Home.dart
+++ lib/Views/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutternews/Helpers/data.dart';
+import 'package:flutternews/Models/CategoryModel.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Home extends StatefulWidget {
@@ -9,6 +11,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategoryModel> categoryModel = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    categoryModel = getCategory();
+    print(categoryModel);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +41,66 @@ class _HomeState extends State<Home> {
           ],
         ),
         elevation: 0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              height: 70,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categoryModel.length,
+                  itemBuilder: (context, index) {
+                    return CategoryTile(
+                      categoryTitle: categoryModel[index].categoryTitle,
+                      imageUrl: categoryModel[index].imageUrl,
+                    );
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final imageUrl, categoryTitle;
+  const CategoryTile({Key? key, this.imageUrl, this.categoryTitle})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 5, right: 5),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              imageUrl,
+              width: 120,
+              height: 60,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            width: 120,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.black.withOpacity(0.5),
+            ),
+            child: Text(
+              categoryTitle,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.nunito(color: Colors.white),
+            ),
+          ),
+        ],
       ),
     );
   }
